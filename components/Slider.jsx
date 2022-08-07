5b7296c7d2a447f45802305bab4052655fd5b7f8
ismailinayat <ismailinayat@gmail.com>
2022-08-07 20:02:41 +0500

    added text on slides

--- components/Slider.jsx
+++ components/Slider.jsx
@@ -14,17 +14,33 @@ function HeaderSlider() {
   return (
     <div className='slider__container'>
     <Slider {...settings}>
-        <div>
+        <div className='single__slide'>
+            
             <div className="image"><img src="night.jpg" alt="" /></div>
+            <div className="slide__text">
+              <h3>WELCOME TO Thawakal transport</h3>
+            </div>
         </div>
-        <div>
+        <div className='single__slide'>
         <div className="image"><img src="modern.jpg" alt="" /></div>
+        <div className="slide__text">
+              <h3>Who we are</h3>
+              <p>We are a International Transport service company having Service across INDIA & UAE.</p>
+            </div>
         </div>
-        <div>
+        <div className='single__slide'>
         <div className="image"><img src="logistic.jpg" alt="" /></div>
+        <div className="slide__text">
+              <h3>What we do</h3>
+              <p>Rent A Car, School And College Transportation, Passenger Transporting, Rental Buses, Tours, And Desert Safari Etc. Rent A Car, School And College Transportation, Passenger Transporting, Rental Buses, Tours, And Desert Safari Etc.</p>
+            </div>
         </div>
-        <div>
+        <div className='single__slide'>
         <div className="image"><img src="school.jpg" alt="" /></div>
+        {/* <div className="slide__text">
+              <h3>WELCOME TO ROADIEZ TRANSORT</h3>
+              <p>Founded Since 2020</p>
+            </div> */}
         </div>
 
     </Slider>
